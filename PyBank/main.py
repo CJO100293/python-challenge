b1f9c37cf53e7ca2a2c56fbf7d2689f5e1c3ba42
CJO100293 <codyjamesosieczonek@outlook.com>
2023-04-16 15:07:53 -0700

    updated 'PyBank/main.py' and 'PyBank/analysis/output.txt'

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,26 +1,23 @@
+#modules
 import os
 import csv
 
+#Path of CSV file
 budget_data = os.path.join("Resources", "budget_data.csv")
 
-# Lists to store data
+#Lists to store data
 Profits = []
-AverageChange = []
-GreatestIncProfitDate = []
-GreatestIncProfitVal= []
-GreatestDecProfitDate = []
-GreatestDecProfitVal = []
-changelist = []
 
-#Initializing values at zero
-TotalMonths = 0
-TotalProfitsLoss = 0
-value = 0
-change = 0
-
+#Reading csv file
 with open(budget_data) as csvfile:
     name = csv.reader(csvfile, delimiter=',')
     next(name,None)
+
+    #Initializing values at zero
+    TotalMonths = 0
+    TotalProfitsLoss = 0
+
+    #Starting for loop
     for row in name:
             #Date
             (row[0])
@@ -32,16 +29,15 @@ with open(budget_data) as csvfile:
             Profits.append(int(row[1]))
             TotalProfitsLoss = TotalProfitsLoss + int(row[1])
 
-            #Average of Profits/Losses
-    
+            #Calculating the average change of monthly profits and losses
+
+            #Calculating the greatest changes of the monthly profits and losses 
 
 #Show On Screen Output
 print('Financial Analysis')
 print('-----------------------------------------------------')
 print(f'Total Months: {TotalMonths}')
 print(f'Total: ${TotalProfitsLoss}')
-#print(f'Average Change: ${avgchange}')
-
 
 #Output data to text file
 with open('analysis/output.txt', 'w') as f:
