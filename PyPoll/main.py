4202a529e81ed478aaf2c037a722a8d7c7c827f4
CJO100293 <codyjamesosieczonek@outlook.com>
2023-04-18 12:30:29 -0700

    updated 'PyPoll/main.py'

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -7,6 +7,7 @@ election_data = os.path.join("Resources", "election_data.csv")
 
 #Lists to store data
 candidate_list = ["Charles Casper Stockham", "Diana DeGette", "Raymon Anthony Doane"]
+candidate_votes = [0,0,0]
 
 #Reading csv file
 with open(election_data) as csvfile:
@@ -14,15 +15,26 @@ with open(election_data) as csvfile:
     next(name,None)
 
     #Initializing values of variables
-    total_votes = sum(1 for row in name)
+    total_votes = 0
 
     #Starting for loop
     for row in name:
-        #Candidate List
-        candidate_list = row[2]
+        #Counting Total Votes
+        total_votes = total_votes + 1
+        #Candidate List + calculating total votes per candidate
+        if candidate_list[0] == row[2]:
+            candidate_votes[0] += 1
+        if candidate_list[1] == row[2]:
+            candidate_votes[1] += 1
+        if candidate_list[2] == row[2]:
+            candidate_votes[2] += 1
 
 #Show On Screen Output
 print('Election Results')
 print('-----------------------------------------------------')
 print(f'Total Votes: {total_votes}')
+print('-----------------------------------------------------')
+print(f'{candidate_list[0]}: more stuff here ({candidate_votes[0]})')
+print(f'{candidate_list[1]}: more stuff here ({candidate_votes[1]})')
+print(f'{candidate_list[2]}: more stuff here ({candidate_votes[2]})')
 print('-----------------------------------------------------')
