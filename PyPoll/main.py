3db1fe7477e808003e9018a901a0fb3f1c75ec62
CJO100293 <codyjamesosieczonek@outlook.com>
2023-04-18 11:44:30 -0700

    updated 'PyPoll/main.py'

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -7,8 +7,6 @@ election_data = os.path.join("Resources", "election_data.csv")
 
 #Lists to store data
 candidate_list = ["Charles Casper Stockham", "Diana DeGette", "Raymon Anthony Doane"]
-winner = []
-
 
 #Reading csv file
 with open(election_data) as csvfile:
@@ -16,9 +14,15 @@ with open(election_data) as csvfile:
     next(name,None)
 
     #Initializing values of variables
-    total_votes = 0
-    percentage = 0
-    candidate_total_votes = 0
-    candidate_win_votes = 0
+    total_votes = sum(1 for row in name)
 
     #Starting for loop
+    for row in name:
+        #Candidate List
+        candidate_list = row[2]
+
+#Show On Screen Output
+print('Election Results')
+print('-----------------------------------------------------')
+print(f'Total Votes: {total_votes}')
+print('-----------------------------------------------------')
